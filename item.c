6587be339a759a6f9a201457e0be68cb335f69bf
ELias <elias.mosco2552@gmail.com>
2025-04-18 08:55:23 -0600

    testing

---
+++ item.c
@@ -0,0 +1,12 @@
+#include"stdio.h"
+#include"item.h"
+#include"stdlib.h"
+
+
+
+void add_item(Item *item_list, double price, char *sku, char *category, char *name, int index){
+
+}
+void free_items(Item *item_list, int size);
+double average_price(Item *item_list, int size);
+void print_items(Item *item_list, int size);
